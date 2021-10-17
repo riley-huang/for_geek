d616a0e35cd9e730c77b3474bf2e36a40d604f9e
riley <riley_huang@163.com>
2021-10-17 17:17:31 +0800

    submit geek homework for week 2

--- Homework2_1.java
+++ Homework2_1.java
@@ -3,19 +3,26 @@ class Homework2_1 {
         List<String> ans = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
         for(String domain : cpdomains){
+            //由于每一个完整的网站域名的访问次数和网站域名是用空格隔开的，所以先将两者分开并用新数组装填
             String[] countNAddress = domain.split("\\s+");
+            //索引为0的就是访问次数
             int count = Integer.valueOf(countNAddress[0]);
+            //索引为1的就是网站域名，然后由于每个子域名都是用“.”隔开的所以再将它们分开并用新数组装填
             String[] address = countNAddress[1].split("\\.");
+            //从最后面的顶级域名开始往前遍历，每次更新完子域名的访问次数后就与前面的子域名做连接，
+            //所以提前准备好一个变量名为str
             String str = "";
             for(int i = address.length - 1; i >= 0; i--){
+                //中间的(i < address.length - 1 ? "." : "")判断是否是最后面的顶级域名，再往后遍历就用不到了
                 str = address[i] + (i < address.length - 1 ? "." : "") + str;
                 if(!map.containsKey(str)){
-                    map.put(str, count);
+                    map.put(str, count);//第一次出现的子域名的话就直接存入容器中
                 }else{
-                    map.put(str, map.get(str) + count);
+                    map.put(str, map.get(str) + count);//之前出现过的子域名就只需更新下访问次数
                 }
             }
         }
+        //遍历添加结果到提前准备好的ans容器里
         for(Map.Entry<String, Integer> entry : map.entrySet()){
             String element = entry.getValue() + " " + entry.getKey();
             ans.add(element);
--- Homework2_2.java
+++ Homework2_2.java
@@ -1,21 +1,22 @@
 class Homework2_2 {
     public int findShortestSubArray(int[] nums) {
-        int most = 1;
+        int most = 1;//度最小值为1
         Map<Integer, int[]> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             if(!map.containsKey(nums[i])){
+                //第一个i是第一次出现该元素的index，第二个i是最后一次出现该元素的index（后续在else语句中会更新）
                 map.put(nums[i], new int[]{1, i, i});
             }else{
                 map.get(nums[i])[2] = i;
                 if(++map.get(nums[i])[0] > most){
-                    most = map.get(nums[i])[0];
+                    most = map.get(nums[i])[0];//更新度的最大值
                 }
             }
         }
-        int ans = nums.length;
-        for(Map.Entry<Integer, int[]> entry : map.entrySet()){
-            if(entry.getValue()[0] == most){
-                ans = Math.min(ans, entry.getValue()[2] - entry.getValue()[1]);
+        int ans = nums.length;//最终答案不会超过总的数组的长度（ans初始值的原因），后续判断时取最小的值返回结果
+        for(int[] value : map.values()){
+            if(value[0] == most){
+                ans = Math.min(ans, value[2] - value[1]);
             }
         }
         return ans + 1;
